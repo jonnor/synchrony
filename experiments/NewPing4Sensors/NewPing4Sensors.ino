4affdee2808c06fbeda994a0541a2d53a62fc79b
Jon Nordby <jononor@gmail.com>
2015-05-08 12:05:27 +0200

    Also turn lights on in Arduino experimentation code

--- experiments/NewPing4Sensors/NewPing4Sensors.ino
+++ experiments/NewPing4Sensors/NewPing4Sensors.ino
@@ -10,10 +10,10 @@ unsigned int cm[SONAR_NUM];         // Where the ping distances are stored.
 uint8_t currentSensor = 0;          // Keeps track of which sensor is active.
 
 NewPing sonar[SONAR_NUM] = {     // Sensor object array.
-  NewPing(11, 12, MAX_DISTANCE), // Each sensor's trigger pin, echo pin, and max distance to ping.
-  NewPing(9, 10, MAX_DISTANCE),
-  NewPing(2, 3, MAX_DISTANCE),
-  NewPing(5, 6, MAX_DISTANCE)
+  NewPing(13+2, 13+1, MAX_DISTANCE), // Each sensor's trigger pin, echo pin, and max distance to ping.
+  NewPing(13+4, 13+3, MAX_DISTANCE),
+  NewPing(13+6, 13+5, MAX_DISTANCE),
+  NewPing(8, 7, MAX_DISTANCE)
 };
 
 void setup() {
@@ -21,6 +21,28 @@ void setup() {
   pingTimer[0] = millis() + 75;           // First ping starts at 75ms, gives time for the Arduino to chill before starting.
   for (uint8_t i = 1; i < SONAR_NUM; i++) // Set the starting time for each sensor.
     pingTimer[i] = pingTimer[i - 1] + PING_INTERVAL;
+    
+
+  pinMode(3, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  /*
+  digitalWrite(3, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, HIGH);
+  */
+//  analogWrite(3, 100);
+  analogWrite(5, 100);
+  analogWrite(6, 100);  
+  analogWrite(9, 100);
+  analogWrite(10, 100);
+  analogWrite(11, 100);
 }
 
 void loop() {
